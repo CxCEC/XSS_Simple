b117dc68390e4d678f8c4cef4088a800b7fca3c1
Chris Merritt <cmerritt@proso.com>
2017-09-11 05:06:28 -0700

    first

---
+++ Documents/Test Code/Simple XSS/index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XSS_Demo
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String s1 = "", s2 = "";
+            try
+            {
+                s1 = Request.QueryString[0]; // unsafe
+				s1 = MyClass.Sanitize(s1);
+            }
+            catch { }
+
+            try
+            {
+                s2 = Request.QueryString[1];
+            }
+            catch { }
+
+            Response.Write(s1 + "<br/>" + System.Web.Security.AntiXss.AntiXssEncoder.HtmlEncode(s2, true));
+
+        }
+    }
+}
